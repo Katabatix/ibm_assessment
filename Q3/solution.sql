fe15a6ff94c7a79851208cd642d0b157d91ea458
Nathan Hui <huinathanc@gmail.com>
2022-11-11 22:21:26 +0800

    Q3 submission

--- Q3/solution.sql
+++ Q3/solution.sql
@@ -1,5 +1,4 @@
-SELECT * FROM category_article_mapping 
-
-SELECT owner_id FROM article
+SELECT * FROM category_article_mapping
+LEFT JOIN article ON category_article_mapping.article_id=article.article_id
 LEFT JOIN owner ON article.owner_id=owner.owner_id
-SELECT 
+SELECT DISTINCT owner_id, owner_name, COUNT(DISTINCT category_id)
